ca00c58820bc954b4e8dc11a5a654f9a8694d222
Mila Nevedomskaya <milo4kamoya@gmail.com>
2023-05-26 08:38:30 -0700

    Console App (while loop)

---
+++ WhileAssignment/WhileAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WhileAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is a do while loop
+            int count = 0;
+            do
+            {
+                Console.WriteLine("Inside the loop! Count = " + count.ToString());
+                count++; // add 1 to count each time
+            }
+            while (count < 10); // continue to loop until count reaches 10
+
+            // this is a while loop
+            int counting = 0;
+            while (counting < 5)
+            {
+                Console.WriteLine("Counting numbers: " + counting.ToString());
+                counting++;
+            }
+            Console.ReadLine();
+        }
+
+
+    }
+}
