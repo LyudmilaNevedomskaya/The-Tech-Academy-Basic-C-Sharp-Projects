320f2f58095e749fcb0ee3718dae9b22271f8302
Mila Nevedomskaya <milo4kamoya@gmail.com>
2023-05-31 14:07:46 -0700

    Exception Handling

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number.");
+                int numOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number.");
+                int numTwo = Convert.ToInt32(Console.ReadLine());
+                int numThree = numOne / numTwo;
+                Console.WriteLine(numOne + " divided by " + numTwo + " equals " + numThree);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
