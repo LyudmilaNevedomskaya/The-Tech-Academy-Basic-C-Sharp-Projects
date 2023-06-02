2d6f2ff868f701cd456af0f1d0485953807fd8bc
Mila Nevedomskaya <milo4kamoya@gmail.com>
2023-06-02 09:19:30 -0700

    Creating Method Overloading

---
+++ MathMethods/MathMethods/MathMethod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMethods
+{
+    public class MathMethod
+    {
+        // method that will take in an integer, create a math operation for this integer (addition, subtraction, etc.), then return the answer as an integer
+        public int AddingNumbers(int number)
+        {
+            return number + 10;
+        }
+
+        // method that will take in a decimal, create a different math operation for it, then return the answer as an integer
+        public int AddingNumbers(decimal number)
+        {
+            int result = Convert.ToInt32(number);
+            return result + 20;
+        }
+
+        // method that will take in a string, create a different math operation for it, then return the answer as an integer
+        public int AddingNumbers(string number)
+        {
+            int result = Convert.ToInt32(number);
+            return result * 5;
+        }
+    }
+}
---
+++ MathMethods/MathMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the MathMethod
+            MathMethod mathMeth = new MathMethod();
+            // method where input as an integer
+            Console.WriteLine(mathMeth.AddingNumbers(5));
+            // method where input as a decimal
+            Console.WriteLine(mathMeth.AddingNumbers(5.5m));
+            // method where input as a string
+            Console.WriteLine(mathMeth.AddingNumbers("5"));
+
+            Console.ReadLine();
+        }
+    }
+}
