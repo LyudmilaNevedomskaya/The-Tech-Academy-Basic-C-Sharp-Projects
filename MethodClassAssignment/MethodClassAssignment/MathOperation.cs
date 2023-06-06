893fbd3c7b06581e0b933333c392124f300d7772
Mila Nevedomskaya <milo4kamoya@gmail.com>
2023-06-06 09:18:44 -0700

    Method Class Assignment

---
+++ MethodClassAssignment/MethodClassAssignment/MathOperation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class MathOperation
+    {
+        // Create a void method that takes two integers as parameters.
+        // Have the method do a math operation on the first integer and display the second integer to the screen
+        public int MathOperation1 (int num1, int num2)
+        {
+            int doMath = num1 * 10;
+            return num2;
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  instantiate the class
+            MathOperation mathOp = new MathOperation();
+
+            // Call the method in the class, passing in two numbers
+            int result = mathOp.MathOperation1(5, 10);
+            Console.WriteLine(result);
+
+            // Call the method in the class, specifying the parameters by name
+            int num1 = 10;
+            int num2 = 20;
+            result = mathOp.MathOperation1(num1, num2);
+            Console.WriteLine(result);
+
+
+            Console.ReadLine();
+        }
+    }
+}
