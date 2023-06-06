f7d7ac8c0896441532802b7a9cf7deeebe9400eb
Mila Nevedomskaya <milo4kamoya@gmail.com>
2023-06-06 13:38:06 -0700

    Methods and Objects Assignment

---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate and initialize an Employee object
+            // with a first name of “Sample” and a last name of “Student”.
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            // Call the superclass method SayName() on the Employee object.
+            employee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
