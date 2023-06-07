abea9864318237ac37f2b3dc858771795b93f2d1
Mila Nevedomskaya <milo4kamoya@gmail.com>
2023-06-07 13:00:27 -0700

    Operators Assignment

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    // Create an Employee class with Id, FirstName and LastName propertie
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property.
+        public static bool operator == (Employee a, Employee b)
+        {
+            if (a.Id == b.Id) return true;
+            else return false;
+        }
+        public static bool operator != (Employee a, Employee b)
+        {
+            if (a.Id != b.Id) return true;
+            else return false;
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  instantiate two objects of the Employee class and assign values to their properties
+            Employee emplOne = new Employee() { Id = 12345, FirstName = "Mila", LastName = "Smith" };
+            Employee emplTwo = new Employee() { Id = 12345, FirstName = "Max", LastName = "Brown" };
+
+            // compare the two Employee objects using the newly overloaded operators and display the results
+            Console.WriteLine(emplOne == emplTwo ? $"{emplOne.FirstName} and {emplTwo.FirstName} have the same ID number: {emplOne.Id}" : $"The IDs are different!");
+
+            Console.ReadLine();
+        }
+    }
+}
