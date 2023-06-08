5e9ff8ec55d9320852ccaa439c0053f9066f56ec
Mila Nevedomskaya <milo4kamoya@gmail.com>
2023-06-08 12:28:04 -0700

    Struct Assignment

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create an object of data type Number and assign an amount to it
+            Number amount = new Number();
+            amount.Amount = 15.5m;
+
+            Console.WriteLine("This is a decimal number: " + amount.Amount);
+            Console.ReadLine();
+        }
+    }
+}
