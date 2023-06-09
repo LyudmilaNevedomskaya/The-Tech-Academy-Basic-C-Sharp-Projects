447cdd49894ea2b3704f25e36f99971f1f7dd6a1
Mila Nevedomskaya <milo4kamoya@gmail.com>
2023-06-09 16:18:45 -0700

    Starting the game

--- TwentyOneGame/TwentyOneGame/Program.cs
+++ TwentyOneGame/TwentyOneGame/Program.cs
@@ -10,43 +10,27 @@ namespace TwentyOneGame
     {
         static void Main(string[] args)
         {
-            //TwentyOneGame game = new TwentyOneGame();
-            //game.Players = new List<string>() { "Mila", "Max", "Yarik" };
-            //game.ListPlayers();
-            //game.Play();
-            //Console.ReadLine();
-
-            //Game game = new TwentyOneGame();
-            //game.Players = new List<Player>();
-            //Player player = new Player();
-            //player.Name = "Mila";
-            //game = game + player;
-
-            Deck deck = new Deck();
-
-            ////// Lambda Expressions ///////////////////////////
-            //int count = deck.Cards.Count(x => x.Face == Face.Ace);
-            //Console.WriteLine(count);
-            //////////////////////////////////////////////////////
-
-            //deck.Shuffle(3);
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name!");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("How much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer =="yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.IsActivePlaying = true;
+                while(player.IsActivePlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+            }
+          
             Console.ReadLine();
         }
         
 
-        //public static Deck Shuffle(Deck deck, int times)
-        //{
-        //    for (int i =0; i < times; i++)
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
     }
 }
