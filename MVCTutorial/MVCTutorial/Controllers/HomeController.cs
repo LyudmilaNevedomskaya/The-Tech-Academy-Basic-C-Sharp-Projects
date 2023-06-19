4d0932e68087d48226fe0286cf975d1ab6767fc5
Mila Nevedomskaya <milo4kamoya@gmail.com>
2023-06-19 12:08:17 -0700

    MVC Tutorial

---
+++ MVCTutorial/MVCTutorial/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using MVCTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //string text = "Hello";
+            //System.IO.File.WriteAllText(@"C:\Users\milo4\Logs\log.txt", text);
+
+            User user = new User();
+            user.Id = 1;
+            user.FName = "Mila";
+            user.LName = "Nevedomskaya";
+            user.Age = 36;
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ NewsletterAppMvc/NewsletterAppMvc/Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using NewsletterAppMvc.Models;
+using NewsletterAppMvc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsletterAppMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(string firstName, string lastName, string emailAddress)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                using (NewsletterEntities db = new NewsletterEntities())
+                {
+                    var signup = new SignUp();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
+
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
+                }
+                // adding data into a database
+                //string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
+                //                        (@FirstName, @LastName, @EmailAddress)";
+                //using (SqlConnection connection = new SqlConnection(connectionString))
+                //{
+                //    SqlCommand command = new SqlCommand(queryString, connection);
+                //    command.Parameters.Add("@FirstName", System.Data.SqlDbType.VarChar);
+                //    command.Parameters.Add("@LastName", System.Data.SqlDbType.VarChar);
+                //    command.Parameters.Add("@EmailAddress", System.Data.SqlDbType.VarChar);
+
+                //    command.Parameters["@FirstName"].Value = firstName;
+                //    command.Parameters["@LastName"].Value = lastName;
+                //    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                //    connection.Open();
+                //    command.ExecuteNonQuery();
+                //    connection.Close();
+
+                //}
+                return View("Success");
+            }
+        }
+
+        //public ActionResult Admin()
+        //{
+            //using (NewsletterEntities db = new NewsletterEntities())
+            //{
+            //    var signups = db.SignUps;
+            //    var signupVms = new List<SignupVm>();
+            //    foreach (var signup in signups)
+            //    {
+            //        var signupVm = new SignupVm();
+            //        signupVm.FirstName = signup.FirstName;
+            //        signupVm.LastName = signup.LastName;
+            //        signupVm.EmailAddress = signup.EmailAddress;
+            //        signupVms.Add(signupVm);
+            //    }
+            //    return View(signupVms);
+            //}
+            // retreiving information from database
+            //string queryString = @"SELECT Id, FirstName, LastName, EmailAddress, SocialSecurityNumber FROM SignUps";
+
+            //List<NewsletterSignUp> signups = new List<NewsletterSignUp>();
+
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    SqlCommand command = new SqlCommand(queryString, connection);
+
+            //    connection.Open();
+
+            //    SqlDataReader reader = command.ExecuteReader();
+
+            //    while (reader.Read())
+            //    {
+            //        var signup = new NewsletterSignUp();
+            //        signup.Id = Convert.ToInt32(reader["Id"]);
+            //        signup.FirstName = reader["FirstName"].ToString();
+            //        signup.LastName = reader["LastName"].ToString();
+            //        signup.EmailAddress = reader["EmailAddress"].ToString();
+            //        signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();
+                    
+            //        signups.Add(signup);
+            //    }
+            //}
+            //var signupVms = new List<SignupVm>();
+            //foreach (var signup in signups)
+            //{
+            //    var signupVm = new SignupVm();
+            //    signupVm.FirstName = signup.FirstName;
+            //    signupVm.LastName = signup.LastName;
+            //    signupVm.EmailAddress = signup.EmailAddress;
+            //    signupVms.Add(signupVm);
+            //}
+            //return View(signupVms);
+        //}
+    }
+}
