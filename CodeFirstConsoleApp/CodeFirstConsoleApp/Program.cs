fc7756586c4357e56370600d3b217239ee4a11e3
Mila Nevedomskaya <milo4kamoya@gmail.com>
2023-06-20 11:35:18 -0700

    Code-First Console App

---
+++ CodeFirstConsoleApp/CodeFirstConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstConsoleApp
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+
+                Console.Write("Student saved successfully!");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
